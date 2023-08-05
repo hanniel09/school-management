97afff1d4501506dd853571e13f479d167d5e95b
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-05 11:40:32 -0300

    feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import {
   ApolloFederationDriver,
   ApolloFederationDriverConfig,
 } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import {
       driver: ApolloFederationDriver,
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
 })
 export class AppModule {}
