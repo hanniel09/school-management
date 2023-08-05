ac4d1f6a0f5a2b261ba0fabb57a0a7b09490ffe4
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-05 11:11:21 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+})
+export class AppModule {}
