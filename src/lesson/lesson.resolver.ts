7c39e087e3485b2be966fcd29b6dd46161d1a0aa
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-05 17:44:50 -0300

    feature

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query } from '@nestjs/graphql';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
@@ -12,4 +13,7 @@ export class LessonResolver {
       endDate: new Date().toISOString(),
     };
   }
+
+  @Mutation((returns) => LessonType)
+  createLession() {}
 }
